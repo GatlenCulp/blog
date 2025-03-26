bd6b9a2e2455d6b8f6106b8a9e0689b112c08ac9
GatlenCulp <GatlenCulp@gmail.com>
2025-03-26 17:21:39 -0400

    fix: Update font, rename philosophy section, move pics to static.

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Gatlen's Blog",
+    pageTitle: "🪿 Gatlen's Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,16 +16,16 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gatlen.blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Fira Sans",
+        body: "Fira Sans",
+        code: "Fira Code",
       },
       colors: {
         lightMode: {
